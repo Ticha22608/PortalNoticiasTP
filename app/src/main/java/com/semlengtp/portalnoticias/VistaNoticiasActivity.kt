c8ea3c6e64c5f6a960e443b023b14e0c1309649f
ticha <ticha22608@outlook.com>
2025-09-08 22:09:52 -0300

    Mejoras en el frontend de la app

--- app/src/main/java/com/semlengtp/portalnoticias/VistaNoticiasActivity.kt
+++ app/src/main/java/com/semlengtp/portalnoticias/VistaNoticiasActivity.kt
@@ -30,17 +30,21 @@ class VistaNoticiasActivity : AppCompatActivity() {
         private fun obtenerNoticias(): List<Noticia> {
             return listOf(
                 Noticia(
-                    titulo = "Las elecciones en la Provincia de Buenos Aires ingresan en la recta final: votó más del 50% del padrón",
-                    descripcion = "Los bonaerenses definen la renovación de la Legislatura y los concejos deliberantes. Los comicios determinarán el equilibrio de poder de Kicillof a nivel local y la proyección de Milei de cara a la votación nacional del mes de octubre",
-                    imagen = "https://www.infobae.com/resizer/v2/QXY7U54ODRADPK7KIVP2Z6DB3A.jpg?auth=e9fceea6f0a922ce10275cae021a3dd5207a682c180e447f0a25b0d5675c562e&smart=true&width=768&height=432&quality=85"
+                    titulo = "Argentinos vence a Lanús por los cuartos de final de la Copa Argentina",
+                    descripcion = "El Bicho y el Granate luchan por el boleto a la fase de los cuatro mejores del torneo. La Copa Argentina entró en su etapa decisiva. Esta noche, en el Cilindro de Avellaneda, Argentinos Juniors y Lanús se ven las caras por un lugar en las semifinales del certamen más federal.",
+                    imagen = "https://www.infobae.com/resizer/v2/KXUQY3W66RHNJLKDU4NAG4GK4I.jpeg?auth=fad3fd1390ee08bc44138152b69e703ede62445f625992d15093b0607afd9ced&smart=true&width=420&height=236&quality=85"
 
                 ),
-                Noticia (
-                    titulo = "Perlitas de las elecciones Buenos Aires 2025, en vivo: un fiscal detenido por robar boletas, otro que quiso abrir una urna con un cuchillo y la madre de Colapinto con el casco de su hijo",
-                    descripcion = "Millones de bonaerense concurrieron hoy a las urnas para la renovación de la Legislatura provincial y de los concejos deliberantes de los 135 municipios. Los hechos curiosos de una jornada clave",
-                    imagen = "https://www.infobae.com/resizer/v2/M2PYDNS3BVFFZMVJ7Z7TI3VXIA.jpg?auth=6bfd1f1f072d34ed6ad269caffbb719e10fa45adcf7843689e8345e1b999e3ce&smart=true&width=992&height=661&quality=85"
+                Noticia(
+                    titulo = "La verdad detrás de la intervención de Gasly en el Alpine de Colapinto tras el GP de Italia de Fórmula 1",
+                    descripcion = "El francés pasó al lado del coche del argentino y operó en los controles. Qué hizo y por qué su gesto terminó con una polémica en las redes sociales",
+                    imagen = "https://www.infobae.com/resizer/v2/2S5AKTSRANA4RFPAGWZPYN3OI4.jpg?auth=4c75425f4f0f2199c0a851c108397e0754f68c30af3c3f129ddb8a6696ccf46b&smart=true&width=420&height=236&quality=85"
+                ),
+                Noticia(
+                    titulo = "Dibu Martínez mostró la intimidad del asado de la Selección y el ritual de bienvenida para un juvenil antes de viajar a Ecuador ",
+                    descripcion = "El arquero campeón del mundo aprovechó para subir un video de la parrilla antes de la cena y el baile que protagonizó Castelau",
+                    imagen = "https://www.infobae.com/resizer/v2/IWPZ2RXDLNERTFKC2EATIUIJ5U.jpg?auth=a3e1c013df01efb3ab2ac7dacd27bcd9a79f8f6634251e1c6b705723dc9f3de0&smart=true&width=420&height=236&quality=85",
                 )
-
             )
         }
     }
