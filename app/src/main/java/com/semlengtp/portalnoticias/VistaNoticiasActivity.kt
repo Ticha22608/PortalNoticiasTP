43042289321e71045eebc04c98ad782f6ccaa32a
OscMoon <moscarag@gmail.com>
2025-10-21 12:47:33 -0300

    Migracion a fragments para noticias y Favoritos. Se agrega fragmentcontainer en activity_noticias_elementos.xml, se crean NoticiasFragment y FavoritosFragment. Ya no se utiliza MisFavoritosActivity.kt

--- app/src/main/java/com/semlengtp/portalnoticias/VistaNoticiasActivity.kt
+++ app/src/main/java/com/semlengtp/portalnoticias/VistaNoticiasActivity.kt
@@ -56,7 +56,7 @@ class VistaNoticiasActivity : AppCompatActivity() {
         toggle.syncState()
         val header = navigationView.getHeaderView(0)
         val cerrarSesion = header.findViewById<ImageButton>(R.id.btnCerrarSesion)
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerNoticias)
+        //val recyclerView = findViewById<RecyclerView>(R.id.recyclerNoticias)
         cerrarSesion.setOnClickListener{
             preferencias.edit().putString(resources.getString(R.string.usuario),null).apply()
             preferencias.edit().putString(resources.getString(R.string.contraseña),null).apply()
@@ -67,11 +67,18 @@ class VistaNoticiasActivity : AppCompatActivity() {
         navigationView.setNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.nav_noticias -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainer, NoticiasFragment())
+                        .commit()
+                    supportActionBar?.title = "Portal Noticias"
                     drawerLayout.closeDrawer(GravityCompat.START)
                     true
                 }
                 R.id.nav_favoritos -> {
-                    startActivity(Intent(this, MisFavoritosActivity::class.java))
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainer, FavoritosFragment())
+                        .commit()
+                    supportActionBar?.title = "Mis favoritos"
                     drawerLayout.closeDrawer(GravityCompat.START)
                     true
                 }
@@ -79,35 +86,16 @@ class VistaNoticiasActivity : AppCompatActivity() {
             }
         }
 
-
-        val noticias = obtenerNoticias()
-
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = NoticiasAdapter(noticias)
-
-    }
-        private fun obtenerNoticias(): List<Noticia> {
-            return listOf(
-                Noticia(
-                    fecha = "8 de Septiembre de 2025",
-                    titulo = "Argentinos vence a Lanús por los cuartos de final de la Copa Argentina",
-                    descripcion = "El Bicho y el Granate luchan por el boleto a la fase de los cuatro mejores del torneo. La Copa Argentina entró en su etapa decisiva. Esta noche, en el Cilindro de Avellaneda, Argentinos Juniors y Lanús se ven las caras por un lugar en las semifinales del certamen más federal.",
-                    imagen = "https://www.infobae.com/resizer/v2/KXUQY3W66RHNJLKDU4NAG4GK4I.jpeg?auth=fad3fd1390ee08bc44138152b69e703ede62445f625992d15093b0607afd9ced&smart=true&width=420&height=236&quality=85"
-
-                ),
-                Noticia(
-                    fecha = "8 de Septiembre de 2025",
-                    titulo = "La verdad detrás de la intervención de Gasly en el Alpine de Colapinto tras el GP de Italia de Fórmula 1",
-                    descripcion = "El francés pasó al lado del coche del argentino y operó en los controles. Qué hizo y por qué su gesto terminó con una polémica en las redes sociales",
-                    imagen = "https://www.infobae.com/resizer/v2/2S5AKTSRANA4RFPAGWZPYN3OI4.jpg?auth=4c75425f4f0f2199c0a851c108397e0754f68c30af3c3f129ddb8a6696ccf46b&smart=true&width=420&height=236&quality=85"
-                ),
-                Noticia(
-                    fecha = "8 de Septiembre de 2025",
-                    titulo = "Dibu Martínez mostró la intimidad del asado de la Selección y el ritual de bienvenida para un juvenil antes de viajar a Ecuador ",
-                    descripcion = "El arquero campeón del mundo aprovechó para subir un video de la parrilla antes de la cena y el baile que protagonizó Castelau",
-                    imagen = "https://www.infobae.com/resizer/v2/IWPZ2RXDLNERTFKC2EATIUIJ5U.jpg?auth=a3e1c013df01efb3ab2ac7dacd27bcd9a79f8f6634251e1c6b705723dc9f3de0&smart=true&width=420&height=236&quality=85",
-                )
-            )
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, NoticiasFragment())
+                .commit()
+            navigationView.setCheckedItem(R.id.nav_noticias)
+            supportActionBar?.title = "Portal Noticias"
         }
 
     }
+
+
+}
